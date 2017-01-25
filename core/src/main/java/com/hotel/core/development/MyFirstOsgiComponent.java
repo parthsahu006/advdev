2951ef1cec4b81def445698363e700ecd83e0c8b
parthsahu006 <parthsahu006@gmail.com>
2017-01-25 17:21:45 +0530

    osgi configuration try

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponent.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponent.java
@@ -1,10 +1,13 @@
 package com.hotel.core.development;
 
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
 
-@Component(metatype=true,label="Enter the website name:",immediate=true)
+@Component(metatype=true,label="Enter the website name:",description = "Simple demo for cron-job like task with properties")
+@Property(name = "scheduler.expression", value = "*/30 * * * * ?",
+description = "Cron-job expression")
 public class MyFirstOsgiComponent {
 
 }
