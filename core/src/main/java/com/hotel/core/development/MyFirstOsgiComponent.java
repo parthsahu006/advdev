66a38109398c22d9e8311e0ab37a90d99f90e32e
parthsahu006 <parthsahu006@gmail.com>
2017-01-25 17:48:27 +0530

    created a service for osgi component

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponent.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponent.java
@@ -1,13 +1,8 @@
 package com.hotel.core.development;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
-
-@Component(metatype=true,label="Enter the website name:",description = "Simple demo for cron-job like task with properties")
 @Service
-
-public class MyFirstOsgiComponent {
+public interface MyFirstOsgiComponent {
 
 }
---
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -0,0 +1,13 @@
+package com.hotel.core.development;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+
+@Component(metatype=true,label="Enter the website name:",description = "Simple demo for cron-job like task with properties")
+@Service(value=MyFirstOsgiComponent.class)
+
+public class MyFirstOsgiComponentImpl implements MyFirstOsgiComponent {
+
+}
