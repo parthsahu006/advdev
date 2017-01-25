8ebfa571fdd9f37135734d2ba4ff3b8b26cbde2e
parthsahu006 <parthsahu006@gmail.com>
2017-01-25 15:37:07 +0530

    created first osgi bundle

---
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponent.java
@@ -0,0 +1,10 @@
+package com.hotel.core.development;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+
+
+@Component(metatype=true,label="Enter the website name:",immediate=true)
+public class MyFirstOsgiComponent {
+
+}
