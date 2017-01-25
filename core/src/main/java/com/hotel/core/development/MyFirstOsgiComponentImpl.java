63f1b46aa4ae3b003aef35a97063a6bbd2e7f439
parthsahu006 <parthsahu006@gmail.com>
2017-01-25 18:32:32 +0530

    added custom @property

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -7,7 +7,8 @@ import org.apache.felix.scr.annotations.Service;
 
 @Component(metatype=true,label="MyFirstOsgiComponent",description = "Simple demo for cron-job like task with properties")
 @Service(value=MyFirstOsgiComponent.class)
-
+@Property(label="tick yes or no:", boolValue=false,
+description = "Whether or not to schedule this task concurrently")
 public class MyFirstOsgiComponentImpl implements MyFirstOsgiComponent {
 
 }
